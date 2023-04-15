23d0cb1e3054ba99683accf1a1e6694a446612cd
Emmanuel Godwin <emmanuel.eze.godwin@gmail.com>
2023-04-15 01:07:51 +0100

    minor changes

--- src/main/java/com/antares/jinsei/JinseiApplication.java
+++ src/main/java/com/antares/jinsei/JinseiApplication.java
@@ -1,9 +1,18 @@
 package com.antares.jinsei;
 
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
 class JinseiApplication {
+
 	public static void main(String... args) {
 
-		
-		
+		JFrame frame = new JFrame("Jinsei");
+		frame.setSize(400, 700);
+		frame.setLocation(1200, 100);
+		frame.setMinimumSize(new Dimension(400, 700));
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
 	}
 }
